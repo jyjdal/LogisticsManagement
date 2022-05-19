66e05b6ff5d1ee1767a722839de1ef3d7a74a9f6
九月家的敖烈 <1604070358@qq.com>
2022-05-19 19:37:35 +0800

    refactor(Backend): 清理代码
    1. 删除用于验证的、无用的代码

--- app/src/main/java/com/example/logisticsmanagement/web/ApiInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.logisticsmanagement.web
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiInterface {
-    @GET("1w")
-    fun getData(): Call<WebData>
-}
--- app/src/main/java/com/example/logisticsmanagement/web/WebData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.logisticsmanagement.web
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class WebData(
-    @JsonProperty("data") val data: String
-)
